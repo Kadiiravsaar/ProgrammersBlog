42d46d0d4a6f351c31b3e9976de407959286cab6
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-30 17:59:41 +0300

    Category class Added

---
+++ ProgBlog.Entites/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using ProgBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBlog.Entites.Concrete
+{
+    public class Category : EntityBase,IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; } // makaleler postlar
+    }
+}
