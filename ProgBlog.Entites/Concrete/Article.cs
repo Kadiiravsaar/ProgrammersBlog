9bac2e7cf506637b875e755f30ab45dc50f6445a
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-30 18:05:02 +0300

    Article class added

---
+++ ProgBlog.Entites/Concrete/Article.cs
@@ -0,0 +1,29 @@
+﻿using ProgBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBlog.Entites.Concrete
+{
+    public class Article: EntityBase, IEntity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Thumbnail { get; set; }
+        public DateTime Date { get; set; }
+        public int ViewsCount { get; set; } // Okunma sayısı
+        public int CommentCount { get; set; } // Yorum sayısı
+        public string SeoAuthor { get; set; } // kim yazdı
+        public string SeoDescription { get; set; } 
+        public string SeoTags { get; set; } // Etiketler
+
+        public int CategoryId { get; set; } 
+        public Category Category { get; set; } // navigation prop => kategoriye ulaşmak için bu şart
+
+        public int UserId { get; set; }
+        public User User { get; set; } // bir makalede bir yazar
+
+    }
+}
