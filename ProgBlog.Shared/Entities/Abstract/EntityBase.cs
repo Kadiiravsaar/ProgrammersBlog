8b27ef5adf73dd562b74898afba614e33336b78b
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-30 17:43:19 +0300

    Add project files.

---
+++ ProgBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase // temel değerler diğer sınıflarda değişsin istersek diye abs kullandık
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual bool IsActive { get; set; } = false;
+        public virtual string CreatedByName { get; set; }
+        public virtual string ModifiedByName { get; set; }
+
+    }
+}
