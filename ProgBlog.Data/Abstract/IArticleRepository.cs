cc7582e849f0f0831919db8e179f6da70e07fd45
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-02 13:25:00 +0300

    10 IEntityRepo implementasyon yapıldı

---
+++ ProgBlog.Data/Abstract/IArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProgBlog.Entites.Concrete;
+using ProgBlog.Shared.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBlog.Data.Abstract
+{
+    public interface IArticleRepository : IEntityRepository<Article>
+    {
+    }
+}
---
+++ ProgBlog.Data/Abstract/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using ProgBlog.Entites.Concrete;
+using ProgBlog.Shared.Data.Abstract;
+
+namespace ProgBlog.Data.Abstract
+{
+    public interface ICategoryRepository : IEntityRepository<Category>
+    {
+    }
+}
