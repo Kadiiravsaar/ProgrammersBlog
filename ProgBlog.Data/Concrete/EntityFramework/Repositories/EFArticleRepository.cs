fa7d0825a1bf3ca8067a4f11c14d96adc5f8756b
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-11 16:57:04 +0300

    .

---
+++ ProgBlog.Data/Concrete/EntityFramework/Repositories/EFArticleRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgBlog.Data.Abstract;
+using ProgBlog.Entites.Concrete;
+using ProgBlog.Shared.Data.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBlog.Data.Concrete.EntityFramework.Repositories
+{
+    public class EFArticleRepository : EfEntityRepositoryBase<Article>, IArticleRepository
+    {
+        public EFArticleRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ ProgBlog.Data/Concrete/EntityFramework/Repositories/EFUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgBlog.Data.Abstract;
+using ProgBlog.Entites.Concrete;
+using ProgBlog.Shared.Data.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBlog.Data.Concrete.EntityFramework.Repositories
+{
+    public class EFUserRepository : EfEntityRepositoryBase<User>, IUserRepository
+    {
+        public EFUserRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
