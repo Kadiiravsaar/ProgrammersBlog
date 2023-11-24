de5ced9f801abcd7b962497070977174a1e44359
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-24 20:51:40 +0300

    IUnitOfWork and UnitOfWork Added

---
+++ ProgBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBlog.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IArticleRepository Articles { get; } // unitOfWork.Articles
+        ICategoryRepository Categories { get; } // unitOfWork.Categories
+        ICommentRepository Comments { get; } // unitOfWork.Comments
+        IUserRepository Users { get; } // unitOfWork.Users
+        IRoleRepository Roles { get; } // unitOfWork.Users
+
+        Task<int> SaveAsync(); // etkilenen kayıt sayılarını almak isterrsem diye <int>
+
+        // unitOfWork.Users.AddAsync(user);
+        // unitOfWork.Categories.AddAsync(category);
+        // unitOfWork.SaveAsync();
+;    }
+}
