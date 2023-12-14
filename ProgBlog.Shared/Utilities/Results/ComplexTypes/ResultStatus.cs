102d9fcee65a76188cb6e5e16840bcfa322099c1
kadiiravsaar <kadiiravsar@gmail.com>
2023-12-14 12:49:31 +0300

    Result Added

---
+++ ProgBlog.Shared/Utilities/Results/ComplexTypes/ResultStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBlog.Shared.Utilities.Results.ComplexTypes
+{
+    public enum ResultStatus
+    {
+        Success = 0,
+        Error = 1,
+        Warning = 2, // ResultStatus.Warning
+        Info = 3
+    }
+}
+
---
+++ ProgBlog.Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,37 @@
+﻿using ProgBlog.Shared.Utilities.Results.Abstract;
+using ProgBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgBlog.Shared.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+
+        public Result(ResultStatus resultStatus,string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+        public Result(ResultStatus resultStatus, string message,Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Exception = exception;
+        }
+
+        public ResultStatus ResultStatus { get; }
+
+        public string Message { get; }  // new Result(ResultStatus.Error,"İşlem başarısız")
+
+        public Exception Exception { get; }
+    }
+}
