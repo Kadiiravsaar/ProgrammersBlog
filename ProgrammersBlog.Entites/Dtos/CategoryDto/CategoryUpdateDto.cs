6d08d0dd6ba41269f92d7945a64c74d939801e80
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-17 06:28:08 +0300

    CategoryDto revised

--- ProgrammersBlog.Entites/Dtos/CategoryDto/CategoryUpdateDto.cs
+++ ProgrammersBlog.Entites/Dtos/CategoryDto/CategoryUpdateDto.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ProgrammersBlog.Entites.Dtos.Category
+namespace ProgrammersBlog.Entites.Dtos.CategoryDto
 {
-    public class CategoryAddDto
+    public class CategoryUpdateDto
     {
+        [Required]
+        public int Id { get; set; }
+
         [DisplayName("Kategori Adı")]
         [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
         [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
@@ -29,6 +27,10 @@ namespace ProgrammersBlog.Entites.Dtos.Category
         [DisplayName("Aktif Mi?")]
         [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
         public bool IsActive { get; set; }
-    }
 
+        [DisplayName("Silindi Mi?")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        public bool IsDeleted { get; set; }
+
+    }
 }
