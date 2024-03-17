d9d1cc0ff7632bc6bf66276d1a1dee1571ea0412
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-17 06:27:51 +0300

    ArticleDto folder Added

---
+++ ProgrammersBlog.Entites/Dtos/ArticleDto/ArticleDto.cs
@@ -0,0 +1,16 @@
+﻿using ProgrammersBlog.Entites.Concrete;
+using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entites.Dtos.ArticleDto
+{
+    public class ArticleDto : DtoGetBase
+    {
+        public Article Article { get; set; }
+    }
+    
+}
---
+++ ProgrammersBlog.Entites/Dtos/ArticleDto/ArticleListDto.cs
@@ -0,0 +1,11 @@
+﻿using ProgrammersBlog.Entites.Concrete;
+using ProgrammersBlog.Shared.Entities.Abstract;
+
+namespace ProgrammersBlog.Entites.Dtos.ArticleDto
+{
+    public class ArticleListDto : DtoGetBase
+    {
+        public IList<Article> Articles { get; set; }
+    }
+
+}
