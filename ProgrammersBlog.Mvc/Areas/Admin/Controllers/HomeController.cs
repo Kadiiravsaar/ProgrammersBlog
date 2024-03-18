ceb6daacefcedead7a0fb3a29c87d233111ba2d9
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-18 07:12:31 +0300

    Admin Area added

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Mvc.Models;
+using System.Diagnostics;
+
+namespace ProgrammersBlog.Mvc.Areas.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+      
+    }
+}
