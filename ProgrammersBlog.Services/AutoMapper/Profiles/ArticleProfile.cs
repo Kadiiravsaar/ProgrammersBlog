5a31043ea9c50fa6f8f40ad664fcc2e3462123cb
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-19 07:28:00 +0300

    used AutoMapper in ArticleProfile and CategoryProfile classes.

---
+++ ProgrammersBlog.Services/AutoMapper/Profiles/ArticleProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using ProgrammersBlog.Entites.Concrete;
+using ProgrammersBlog.Entites.Dtos.ArticleDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.AutoMapper.Profiles
+{
+    public class ArticleProfile:Profile
+    {
+        public ArticleProfile()
+        {
+            // adddto içinde createdDate yok ancak bunu aşağıdaki gibi yapacağız
+
+
+            // ArticleAddDto al ve article'ye dönüştür
+            CreateMap<ArticleAddDto, Article>().ForMember(dest=>dest.CreatedDate,opt=>opt.MapFrom(x=>DateTime.UtcNow)); 
+            CreateMap<ArticleUpdateDto, Article>().ForMember(dest => dest.ModifiedByName, opt => opt.MapFrom(x => DateTime.UtcNow));
+        }
+
+
+    }
+}
---
+++ ProgrammersBlog.Services/AutoMapper/Profiles/CategoryProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ProgrammersBlog.Entites.Concrete;
+using ProgrammersBlog.Entites.Dtos.CategoryDto;
+
+namespace ProgrammersBlog.Services.AutoMapper.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            // adddto içinde createdDate yok ancak bunu aşağıdaki gibi yapacağız
+
+
+            // CategoryAddDto al ve Category'ye dönüştür
+            CreateMap<CategoryAddDto, Category>().ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(x => DateTime.UtcNow));
+            CreateMap<CategoryUpdateDto, Category>().ForMember(dest => dest.ModifiedByName, opt => opt.MapFrom(x => DateTime.UtcNow));
+        }
+
+
+    }
+}
