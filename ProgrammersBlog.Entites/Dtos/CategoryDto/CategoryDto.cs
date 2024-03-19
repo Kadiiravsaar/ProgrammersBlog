b2225f2ff459d186b594f573f9d9a41096ab581d
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-19 07:20:52 +0300

    CategoryListDto and  CategoryDto added

---
+++ ProgrammersBlog.Entites/Dtos/CategoryDto/CategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using ProgrammersBlog.Entites.Concrete;
+using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entites.Dtos.CategoryDto
+{
+    public class CategoryDto : DtoGetBase
+    {
+        public Category Category{ get; set; }
+    }
+}
---
+++ ProgrammersBlog.Entites/Dtos/CategoryDto/CategoryListDto.cs
@@ -0,0 +1,10 @@
+﻿using ProgrammersBlog.Entites.Concrete;
+using ProgrammersBlog.Shared.Entities.Abstract;
+
+namespace ProgrammersBlog.Entites.Dtos.CategoryDto
+{
+    public class CategoryListDto : DtoGetBase
+    {
+        public IList<Category> Categories { get; set; }
+    }
+}
