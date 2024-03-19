dcf8a0ad5fef5112b9f64f7a7fec702d955c9733
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-19 07:27:30 +0300

    Implemented Dependency Injection in LoadMyService method

---
+++ ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Data.Concrete;
+using ProgrammersBlog.Data.Concrete.EntityFramework.Contexts;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Services.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyService(this IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<IArticleService, ArticleManager>();
+
+            return services;
+        }
+    }
+}
