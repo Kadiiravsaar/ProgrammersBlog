11f38997d30b35a4f5a9a384399832e677e0ea5b
kadiiravsaar <kadiiravsar@gmail.com>
2024-03-27 07:59:18 +0300

    site.js Created site.js file and added convertFirstLetterToUpperCase, convertToShortDate methods

---
+++ ProgrammersBlog.Mvc/wwwroot/Temp/js/site.js
@@ -0,0 +1,8 @@
+﻿function convertFirstLetterToUpperCase(text) { 
+    // ilk harfi büyük harfe dönüitür
+    return text.charAt(0).toUpperCase() + text.slice(1); // ilk harfi al.büyük harfe çevir + tüm cümleyi geri dön
+}
+function convertToShortDate(dateString) { // saati düzenlemek için createdDate and modifiedDate
+    const shortDate = new Date(dateString).toLocaleDateString('en-US');
+    return shortDate;
+}
