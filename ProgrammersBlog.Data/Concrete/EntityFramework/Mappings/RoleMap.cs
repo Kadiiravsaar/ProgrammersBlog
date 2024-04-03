67e48bace0142aea6acf4d7fe2854465529a1fd5
kadiiravsaar <kadiiravsar@gmail.com>
2024-04-03 07:04:49 +0300

    Identity Establishing Identity structure 1.2

--- ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/RoleMap.cs
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/RoleMap.cs
@@ -8,47 +8,30 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
     {
         public void Configure(EntityTypeBuilder<Role> builder)
         {
+            // Primary key
             builder.HasKey(r => r.Id);
-            builder.Property(r => r.Id).ValueGeneratedOnAdd(); // ıdentity alanı
 
-            builder.Property(r => r.Name).IsRequired();
-            builder.Property(r => r.Name).HasMaxLength(30);
+            // Index for "normalized" role name to allow efficient lookups
+            builder.HasIndex(r => r.NormalizedName).HasDatabaseName("RoleNameIndex").IsUnique();
 
-            builder.Property(r => r.Description).IsRequired();
-            builder.Property(r => r.Description).HasMaxLength(250);
+            // Maps to the AspNetRoles table
+            builder.ToTable("AspNetRoles");
 
+            // A concurrency token for use with the optimistic concurrency checking
+            builder.Property(r => r.ConcurrencyStamp).IsConcurrencyToken(); 
 
+            // Limit the size of columns to use efficient database types
+            builder.Property(u => u.Name).HasMaxLength(100);
+            builder.Property(u => u.NormalizedName).HasMaxLength(100);// büyük harflere çevrildiği ve indexlendiği kısımdır
 
-            builder.Property(r => r.CreatedByName).IsRequired();
-            builder.Property(r => r.CreatedByName).HasMaxLength(50);
+            // The relationships between Role and other entity types
+            // Note that these relationships are configured with no navigation properties
 
-            builder.Property(r => r.ModifiedByName).IsRequired();
-            builder.Property(r => r.ModifiedByName).HasMaxLength(50);
+            // Each Role can have many entries in the UserRole join table
+            builder.HasMany<UserRole>().WithOne().HasForeignKey(ur => ur.RoleId).IsRequired();
 
-            builder.Property(r => r.CreatedDate).IsRequired();
-
-            builder.Property(r => r.ModifiedDate).IsRequired();
-
-            builder.Property(r => r.IsActive).IsRequired();
-
-            builder.Property(r => r.IsDeleted).IsRequired();
-
-            builder.Property(r => r.Note).HasMaxLength(500); // nul geçilebilir
-
-            builder.ToTable("Roles");
-            builder.HasData(new Role
-            {
-                Id = 1,
-                Name = "Admin",
-                Description = "Admin Rolü, Tüm Haklara Sahiptir.",
-                IsActive = true,
-                IsDeleted = false,
-                CreatedByName = "InitialCreate",
-                CreatedDate = DateTime.Now,
-                ModifiedByName = "InitialCreate",
-                ModifiedDate = DateTime.Now,
-                Note = "Admin Rolüdür."
-            });
+            // Each Role can have many associated RoleClaims
+            builder.HasMany<RoleClaim>().WithOne().HasForeignKey(rc => rc.RoleId).IsRequired();
         }
 
     }
