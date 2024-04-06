5031a815d45c995096d15d398f6683ec038ba61a
kadiiravsaar <kadiiravsar@gmail.com>
2024-04-06 04:48:17 +0300

    DateTimeExtensions Added DateTime extension method for image upload operations

---
+++ ProgrammersBlog.Shared/Utilities/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string FullDateAndTimeStringWithUnderscore(this DateTime dateTime)
+        {
+            return
+                $"{dateTime.Hour}_{dateTime.Day}_{dateTime.Month}_{dateTime.Year}";
+        }
+    }
+}
