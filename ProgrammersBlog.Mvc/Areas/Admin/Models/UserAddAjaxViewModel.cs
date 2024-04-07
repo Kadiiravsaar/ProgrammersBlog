afa8ec8302b05a3e095a8d5d82f5df6947a1890f
kadiiravsaar <kadiiravsar@gmail.com>
2024-04-07 05:29:25 +0300

    User add User addition operation completed

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Models/UserAddAjaxViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ProgrammersBlog.Entities.Dtos.CategoryDto;
+using ProgrammersBlog.Entities.Dtos.UserDto;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Models
+{
+    public class UserAddAjaxViewModel
+    {
+        public UserDto UserDto { get; set; }
+        public UserAddDto UserAddDto { get; set; }
+        public string UserAddPartial { get; set; }
+    }
+}
